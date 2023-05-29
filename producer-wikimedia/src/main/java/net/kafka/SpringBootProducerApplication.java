967ab845637072d506decc9ebbd4a0a8ff5d1efe
Harmeet Singh Gorowada <harmeetsingh7276@gmail.com>
2023-05-29 17:05:27 +0530

    added new topic

--- producer-wikimedia/src/main/java/net/kafka/SpringBootProducerApplication.java
+++ producer-wikimedia/src/main/java/net/kafka/SpringBootProducerApplication.java
@@ -6,7 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringBootProducerApplication {
     public static void main(String[] args) {
-
         SpringApplication.run(SpringBootProducerApplication.class);
     }
 }
