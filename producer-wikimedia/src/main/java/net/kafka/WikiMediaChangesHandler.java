644808b58b9642c980ec7e62701cea2836184cdd
Harmeet Singh Gorowada <harmeetsingh7276@gmail.com>
2023-05-29 17:24:43 +0530

    added wikimedia handler changes

---
+++ producer-wikimedia/src/main/java/net/kafka/WikiMediaChangesHandler.java
@@ -0,0 +1,46 @@
+package net.kafka;
+
+import com.launchdarkly.eventsource.EventHandler;
+import com.launchdarkly.eventsource.MessageEvent;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+
+public class WikiMediaChangesHandler implements EventHandler {
+    private static final Logger LOGGER = LoggerFactory.getLogger(WikiMediaChangesProducer.class);
+    private KafkaTemplate<String, String> kafkaTemplate;
+
+    public WikiMediaChangesHandler(KafkaTemplate<String, String> kafkaTemplate, String topic) {
+        this.kafkaTemplate = kafkaTemplate;
+        this.topic = topic;
+    }
+
+    private String topic;
+
+    @Override
+    public void onOpen() throws Exception {
+
+    }
+
+    @Override
+    public void onClosed() throws Exception {
+
+    }
+
+    @Override
+    public void onMessage(String s, MessageEvent messageEvent) throws Exception {
+        //Whenever there is a new event in wikimedia this message is invoked
+        LOGGER.info(String.format("event data -> %s",messageEvent.getData()));
+        kafkaTemplate.send(topic,messageEvent.getData());
+    }
+
+    @Override
+    public void onComment(String s) throws Exception {
+
+    }
+
+    @Override
+    public void onError(Throwable throwable) {
+
+    }
+}
